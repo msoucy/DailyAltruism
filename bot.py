045687fc4342ed3cf19f9bc10b8bc1745570cddd
Matt Soucy <msoucy@csh.rit.edu>
2018-11-01 19:40:31 -0400

    Added comments, multiple lists

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from mastodon import Mastodon
 from random import choice
+import os
 
 masto = Mastodon(
     api_base_url='https://botsin.space',
@@ -8,11 +9,20 @@ masto = Mastodon(
     access_token="access_token.txt"
 )
 
+def readFile(fn):
+    with open(fn) as fi:
+        lines = fi.readlines()
+    lines = (line.strip()
+             for line in lines
+             if not line.startswith("#"))
+    return list(lines)
+
 if __name__ == '__main__':
-    # print("Publishing to Mastodon version", masto.retrieve_mastodon_version())
-    with open("deeds.txt") as fi:
-        deed = choice(fi.readlines()).rstrip()
+    deeds = []
+    for fn in os.listdir("deeds"):
+        deeds.extend(readFile(os.path.join("deeds", fn)))
+    deed = choice(deeds)
     text = "Today's good deed: " + deed
     print("Tooting:", text)
-    masto.status_post(text)
+    # masto.status_post(text)
 
